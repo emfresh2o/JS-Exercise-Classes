4bdc18b31fe4eea93607c89d38e3ecfa8f141d6f
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-04-30 00:48:36 -0400

    2nd commit TASK 2

--- index.js
+++ index.js
@@ -74,6 +74,25 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallon){
+    this.tank += gallons;
+  }
+  drive(distance){
+    if (distance < (this.tank * this.milesPerGallon)) {
+      this.odometer += distance;
+      this.tank -= (distance / this.milesPerGallon);
+    } else {
+      this.odometer += (this.tank * this.milesPerGallon);
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer}`;
+    }
+  }
 
 }
 
